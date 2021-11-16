aedc9fe49a759404c0c18740b358c869097c0dbf
Johannes Welsch <welschmoor@gmail.com>
2021-11-16 23:39:18 +0100

    almost done, local storage added, LS check for array

--- js/todo.js
+++ js/todo.js
@@ -3,13 +3,45 @@
 const todoForm = document.querySelector('.todoform')
 const todoUL = document.querySelector('.todo-ul')
 
+let todoList = []
+
+if (localStorage.getItem('todolist')) {
+  const todoArray = JSON.parse(localStorage.getItem('todolist'))
+
+  // if todolist key returns not an array, we delete everything in local storage
+  if (typeof todoArray !== 'object') {
+    localStorage.removeItem('todolist')
+  }
+
+
+
+  todoArray.forEach((each, i) => {
+    const todoLI = document.createElement('li')
+    console.log("each", each)
+    todoLI.textContent = each
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = "X"
+    todoLI.appendChild(deleteBtn)
+    todoUL.appendChild(todoLI)
+    const deleteHandler = (e) => {
+      e.target.parentElement.remove()
+    }
+    deleteBtn.addEventListener('click', deleteHandler)
+
+  })
+  todoForm.todoinput.value = ''
+}
 
 
 
 const submitTodoHandler = (e) => {
   e.preventDefault()
   const todoLI = document.createElement('li')
-  todoLI.textContent = e.target.elements.todoinput.value.trim()
+  const todoText = e.target.elements.todoinput.value.trim()
+  todoLI.textContent = todoText
+  todoList.push(todoText)
+  localStorage.setItem('todolist', JSON.stringify(todoList))
+
   const deleteBtn = document.createElement('button')
   deleteBtn.textContent = "X"
   todoLI.appendChild(deleteBtn)
@@ -21,5 +53,6 @@ const submitTodoHandler = (e) => {
   deleteBtn.addEventListener('click', deleteHandler)
 
   todoForm.todoinput.value = ''
+  console.log('todolist: ', todoList)
 }
 todoForm.addEventListener('submit', submitTodoHandler)
